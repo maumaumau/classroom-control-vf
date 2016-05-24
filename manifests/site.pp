bb585bb77a274c02ad8d4660b4102e6397b5e5cf
maumaumau <urban.michael.a@gmail.com>
2016-05-24 14:07:02 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,10 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'maumaumau.puppetlabs.vm' {
+notify { "W are on: ${::hostname}": }
+}
+
 node default {
 
 include users
