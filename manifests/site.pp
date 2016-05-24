98ad9f608ecfe1cd0d4e658849720256ae2d98b2
maumaumau <urban.michael.a@gmail.com>
2016-05-24 14:20:40 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,8 +44,8 @@ ini_setting { 'random ordering':
 
 node default {
 
-include users
-include skeleton
+#include users
+#include skeleton
 
   # This is where you can declare classes for all nodes.
   # Example:
