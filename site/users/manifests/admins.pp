87e31b7a539993ec7e2b7c49edf096c225b0c855
maumaumau <urban.michael.a@gmail.com>
2016-05-25 13:27:02 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -2,6 +2,6 @@ class users::admins {
 users::managed_user { 'joe': }
 users::managed_user { 'alice': }
 users::managed_user { 'aaron': }
-users::managed_user { 'alice': }
+users::managed_user { 'ralph': }
 
 }
