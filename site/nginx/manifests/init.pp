40aba1e230236ddef0805b64dce5262945f2ea2d
maumaumau <urban.michael.a@gmail.com>
2016-05-25 08:11:50 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,3 +1,40 @@
 class nginx {
 
+package { 'nginx':
+  ensure => present,
+}
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0664',
+  source  => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
+}
+
+file { '/var/www':
+  ensure  => directory,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0775',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/conf.d/default.conf':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0664',
+  source  => 'puppet:///modules/nginx/default.conf',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+  subscribe => File['/etc/nginx/conf.d/default.conf'],
+}
+
 }
